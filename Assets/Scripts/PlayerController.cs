994743e3fadd95e624ab80aecf0b41b8b88ca4de
Deividson Sabino <xdeividsabinox@live.com>
2023-01-02 21:07:43 -0300

    feat: add collision method and set enemy tag

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,4 +31,12 @@ public class PlayerController : MonoBehaviour
             hasPowerup = true;
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
+    }
 }
