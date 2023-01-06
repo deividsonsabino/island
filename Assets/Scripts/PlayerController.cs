3288da775dc9dad068b7a3165cfb4eb3c2688c7e
Deividson Sabino <xdeividsabinox@live.com>
2023-01-06 12:41:13 -0300

    feat: add smash powerup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,15 +7,24 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     private float powerupStrength = 15.0f;
+
     public float speed = 5.0f;
     public bool hasPowerup = false;
     public GameObject powerupIndicator;
     
     public PowerUpType currentPowerUp = PowerUpType.None;
-
     public GameObject rocketPrefab;
+
     private GameObject tmpRocket;
     private Coroutine powerupCountdown;
+
+    public float hangTime;
+    public float smashSpeed;
+    public float explosionForce;
+    public float explosionRadius;
+
+    private bool smashing = false;
+    private float floorY;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +44,12 @@ public class PlayerController : MonoBehaviour
         if (currentPowerUp == PowerUpType.Rockets && Input.GetKeyDown(KeyCode.F))
         {
             LaunchRockets();
+        }
+
+        if(currentPowerUp == PowerUpType.Smash && Input.GetKeyDown(KeyCode.Space) && !smashing)
+        {
+            smashing = true;
+            StartCoroutine(Smash());
         }
 
         if (transform.position.y < -10)
@@ -88,6 +103,45 @@ public class PlayerController : MonoBehaviour
         {
             tmpRocket = Instantiate(rocketPrefab, transform.position + Vector3.up, Quaternion.identity);
             tmpRocket.GetComponent<RocketBehaviour>().Fire(enemy.transform);
+        }
+    }
+
+    IEnumerator Smash()
+    {
+        var enemies = FindObjectsOfType<Enemy>();
+
+        //Store the y position before taking off
+        floorY = transform.position.y;
+
+        //Calculate the amount of time we will go up
+        float jumpTime = Time.time + hangTime;
+
+        while(Time.time < jumpTime)
+        {
+            //move the player up while still keeping their x velocity.
+            playerRb.velocity = new Vector2(playerRb.velocity.x, smashSpeed);
+            yield return null;
+        }
+
+        //Now move the player down
+        while (transform.position.y > floorY)
+        {
+            playerRb.velocity = new Vector2(playerRb.velocity.x, -smashSpeed * 2);
+            yield return null;
+        }
+
+        //Cycle through all enemies.
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            //Apply an explosion force that originates from our position.
+            if (enemies[i] != null)
+            {
+                enemies[i].GetComponent<Rigidbody>()
+                    .AddExplosionForce(explosionForce, transform.position, explosionRadius, 0.0f, ForceMode.Impulse);
+            }
+
+            //We are no longer smashing, so set the boolean to false
+            smashing = false;
         }
     }
 }
